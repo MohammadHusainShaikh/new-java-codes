f49cf9ab21f074e86ec509473ac571aeabdfb883
MohammadHusainShaikh <mohammad.husain2760@gmail.com>
2025-07-10 21:32:51 +0530

    [ADDED]: Introduction of Data types in java

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int i = 84443;
+        byte b = 127;
+        short s = 5555;
+        long l = 44433L;
+        char c = 'C';
+        float f = 8.4f;
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(s);
+    }
+}
